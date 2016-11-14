11073ef10ef1206ecccafc6414ab0bef58ee5122
Alain Trantik <trantika@coventry.ac.uk>
2016-11-14 15:56:42 +0000

    node delete

--- week5/DoubleLinkedList/DoubleLinkedList/Source.cpp
+++ week5/DoubleLinkedList/DoubleLinkedList/Source.cpp
@@ -1,12 +1,10 @@
 #include <iostream>
 
-//Nodes and lists use public data members for convenience
-//This may make some software engineers froth at the mouth
 
 class Node
 {
 public:
-	int value; //This could really be any type
+	int value; //Any type
 	Node* next;
 	Node* prev;
 	Node(int val){
@@ -17,9 +15,7 @@ public:
 	}
 	~Node(){
 		std::cout << "Node destructor" << std::endl;
-		this->prev=NULL;
-		this->next=NULL;
-		this->value = NULL;
+		std::cout << "I had the value " << this->value << std::endl;
 	}
 };
 class List
@@ -31,34 +27,55 @@ public:
 
 	List(){
 		std::cout << "List Constructor!" << std::endl;
-		this->head = NULL;
-		this->tail = NULL;
+		this->head = 0;
+		this->tail = 0;
 	}
 	~List(){
 		std::cout << "List destructor!" << std::endl;
+		Node* temp;
+		while (head != 0) {
+			temp = head->next;
+			delete head;
+			head = temp;
+		}
 	}
 	void insert(Node* n, Node* x){
-		if (n != NULL){
+		//Not actually perfect: how do we prepend to an existing list?
+		if (n != 0){
 			x->next = n->next;
 			n->next = x;
 			x->prev = n;
 			if (x->next != 0)
 				x->next->prev = x;
 		}
-		if (this->head == NULL){
+		if (this->head == 0){
 			this->head = x;
 			this->tail = x;
-			x->prev = NULL;
-			x->next = NULL;
+			x->prev = 0;
+			x->next = 0;
 		}
 		else if (this->tail = n){
 			this->tail = x;
 		}
 	}
+	void remove(Node* n) {
+		if (n != 0  && this->head != n && this->tail != n) {
+
+			n->next->prev = n->prev;
+			n->prev->next = n->next;
+		}
+		if (this->head == n) {
+			this->head = n->next;
+		}
+		if (this->tail == n) {
+			this->tail = n->prev;
+		}
+		delete n;
+	}
 	void display(){
 		Node* i = this->head;
 		std::cout << "List: ";
-		while (i != NULL){
+		while (i != 0){
 			std::cout << i->value << ",";
 			i = i->next;
 		}
@@ -69,12 +86,13 @@ public:
 int main(int argc, char *argv[])
 {
 	List* l = new List();
-	Node* a = new Node(4);
-	l->insert(NULL, a);
-	l->insert(l->head, new Node(6));
+	Node* a = new Node(6);
+	l->insert(l->head, new Node(4));
+	l->insert(l->head, a);
 	l->insert(l->head, new Node(8));
 	l->display();
-	delete a;
+	l->remove(a);
+	l->display();
 	getchar();
 	return 0;
 }

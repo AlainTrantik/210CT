8884af5fa9986fce4e9b2cd8b8213de7e6a6aadf
Alain Trantik <trantika@coventry.ac.uk>
2016-11-24 12:36:27 +0000

    unweighted graph done

--- week7-Graphs/Graph/Graph/Program.cs
+++ week7-Graphs/Graph/Graph/Program.cs
@@ -10,24 +10,28 @@ namespace Graph
     {
         static void Main(string[] args)
         {
-            Vertex one=new Vertex();
-            one.data = 1;
+            Vertex one=new Vertex(1); //I add 5 nodes without any edges for the moment
             Vertex two = new Vertex(2);
             Vertex three = new Vertex(3);
             Vertex four = new Vertex(4);
+            Vertex five = new Vertex(5);
 
-            one.neigbors.Add(two);
-            one.neigbors.Add(three);
+            one.neigbors.Add(two); //Node 1 is linked to Node 2
 
-            two.neigbors.Add(one);
-            two.neigbors.Add(three);
+            two.neigbors.Add(one); //Node 2 is linked to Node 1 and four ...
+            two.neigbors.Add(four);
 
-            three.neigbors.Add(one);
-            three.neigbors.Add(two);
             three.neigbors.Add(four);
+            three.neigbors.Add(five);
 
-            four.neigbors.Add(three);
+            Vertex[] vertices = { two, three };
+            four.Add(vertices); //I use my function Add to create edges
+            four.neigbors.Add(five);
 
+            five.neigbors.Add(three);
+            five.neigbors.Add(four);
+
+            //Finally I have the unweighted undiricted graph of the page 22 of the lecture on graphs
         }
     }
 }
--- week7-Graphs/Graph/Graph/Vertex.cs
+++ week7-Graphs/Graph/Graph/Vertex.cs
@@ -5,7 +5,9 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Graph
-{
+{/// <summary>
+/// I chose the adjacency list approach so I created a new class Vertex (Node) with a data and a list of vertices representing the neighbors. 
+/// </summary>
     class Vertex
     {
         public int data;
@@ -15,9 +17,44 @@ namespace Graph
         {
         }
 
-        public Vertex(int data)
+        public Vertex(int data)//Constructor to Create/Add a new node
         {
             this.data = data;
         }
+
+        //Getter and Setter
+        public int Data
+        {
+            get
+            {
+                return data;
+            }
+            set
+            {
+                this.data = value;
+            }
+        }
+
+        public List<Vertex> Neighbors
+        {
+            get
+            {
+                return neigbors;
+            }
+            set
+            {
+                this.neigbors = value;
+            }
+        }
+
+        //Add egdes function
+        public void Add(Vertex[] vertices)
+        {
+            for(int i = 0; i < vertices.Length; i++)
+            {
+                this.neigbors.Add(vertices[i]);
+            }
+        }
+
     }   
 }
